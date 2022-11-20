f2c68f8bb9ba400012dff8a918a5adbc00473528
omarfarah123 <76454677+omarfarah123@users.noreply.github.com>
2022-11-19 20:31:57 -0500

    Added Routeway for new Document Model

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const courseRoutes = require('./course-routes');
 const lectureRoutes = require('./lecture-routes')
+const documentRoutes = require('./document-routes');
 
 router.use('/users', userRoutes);
 router.use('/courses', courseRoutes);
-router.use('/lectures', lectureRoutes)
+router.use('/lectures', lectureRoutes);
+router.use('/documents', documentRoutes);
 
 module.exports = router;
