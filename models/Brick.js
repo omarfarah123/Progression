33945ff325f23503bba6d2789cbd064b4d2864f4
omarfarah123 <76454677+omarfarah123@users.noreply.github.com>
2022-11-25 12:36:37 -0500

    Created Brick Model

---
+++ models/Brick.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/conection');
+
+class Brick extends Model {}
+
+Brick.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+        single_brick: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+          },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },  
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'brick',
+    }
+);
+
+module.exports = Brick;
